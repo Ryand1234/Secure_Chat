0b90473d4d5ab5870ef485f609b9e5e8798e064c
Riyan Dhiman <Riyandhiman14@gmail.com>
2020-05-11 23:04:44 +0530

    Added Comments, Code clean

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ var path = require('path')
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var users = {}
-var reverse_users = {}
+var users = {}  //To store username as key and socket id as value;
+		//to retrive to_send user id to send message
+
+var reverse_users = {}  //To store socket id as key and username as value;
+			//to retrive sender name
 var user;
 
 var app = express();
@@ -22,23 +25,22 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
+//To Create Server
 var server = app.listen(process.env.PORT||3000, ()=>{
 	console.log("Server Listening at http://localhost:3000");
 	});
 
 const io = require('socket.io').listen(server);
-/*
-io.configure(function(){
-	io.set('transports', ['xhr-polling']);
-	io.set('polling duration',10);
-});*/
+
+
+//Connection
 io.on('connection', (socket)=>{
 	users[user] = socket.id;
 	reverse_users[socket.id] = user;
 	console.log(user, " Connected");
 	socket.on('client', (msg)=>{
 	
+		//To seperate name from '@'
 		var mentioned_user_with_sign = msg.user;
 		var mentioned_user = mentioned_user_with_sign.split('@')[1]
 
@@ -48,6 +50,8 @@ io.on('connection', (socket)=>{
 			user : reverse_users[socket.id],
 			message : message
 		};
+
+		//To Broadcast message to desire user.
 		if(users[mentioned_user] != undefined)
 			socket.to(users[mentioned_user]).emit('server', send_message)
 	});
@@ -58,11 +62,14 @@ io.on('connection', (socket)=>{
 });
 
 
+
+//To render HomePage i.e., to enter username
 app.get('/', (req, res, next)=>{
 	res.render('home.ejs');
 });
 
 
+//To render Chat HomePage;
 app.get('/chat/enter', (req, res, next)=>{
 
 	user = req.query.user;
